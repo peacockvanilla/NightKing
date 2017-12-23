2bf206dd61786effffb9a2f0bc1000552bc13f48
JonSnow <peacockvanilla@outlook.com>
2017-12-23 08:04:34 -0500

    v1 ui

---
+++ MVC5_full_version/Controllers/WalletController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_full_version.Controllers
+{
+    public class WalletController : Controller
+    {
+        // GET: Wallet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
